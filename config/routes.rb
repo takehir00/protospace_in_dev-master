4b9ea5c259e8f9047135a10f0e03c9fb2ed8d357
MasahiroAsaka <pedestrem.paradiso@gmail.com>
2018-05-11 10:05:28 +0900

    modify_routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'prototypes#index'
-  resources :prototypes, only: [:index, :new, :create, :edit, :update, :show]
+
+  resources :prototypes, only: [:index, :new, :create, :edit, :update, :show] do
+    resources :comments, only: [:create, :edit, :update, :destroy]
+  end
+
+
   resources :users, only: [:show, :edit, :update]
+
 end
