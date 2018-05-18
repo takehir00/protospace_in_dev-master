534b88ba4864c17823133367e7372444e135efe4
motoki0208 <gift0119@gmail.com>
2018-05-18 18:06:19 +0900

    define routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root 'prototypes#index'
   resources :prototypes, only: [:index, :new, :create, :edit, :update, :show]
   resources :users, only: [:show, :edit, :update]
+  resources :tags, only: [:index, :show, :update, :destroy]
 end
