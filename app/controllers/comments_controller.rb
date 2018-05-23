0ab029985e7f11549f55f2cebc411d13b4f5a516
takehir00 <0kutakehiro15@gmail.com>
2018-05-23 11:47:50 +0900

    authenticate_user

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!, only: :create
+
   def create
     @comment=Comment.create(comment_params)
     respond_to do |format|
