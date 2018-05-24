a585bb854bf1b416dfddb4eed71a15486f76cf84
takehir00 <0kutakehiro15@gmail.com>
2018-05-24 11:23:05 +0900

    install_factorygirl

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+describe comment do
+  describe '#create' do
+    it "is valid with a id, body, user_id, prototype_id" do
+      comment =
+      expect().to be_valid
+    end
+  end
+end
