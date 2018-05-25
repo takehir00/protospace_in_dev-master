fdbb34bc13132e4b843bd331c320f5ba9a9809bc
motoki0208 <gift0119@gmail.com>
2018-05-25 10:41:01 +0900

    modify tag-delete function

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -7,14 +7,15 @@ class TagsController < ApplicationController
     @tag = Tag.find(params[:id])
   end
 
-  def update
-  end
-
   def destroy
     @tag = Tag.find(params[:id])
     @prototype = Prototype.find(params[:prototype_id])
-    if @tag.destroy
+    @prototype_tag = PrototypeTag.find_by(tag_id: params[:id], prototype_id: params[:prototype_id])
+    if user_signed_in? && current_user.id == @prototype.user.id
+      @prototype_tag.destroy
       redirect_to prototype_path(@prototype), notice: 'Tag was successfully deleted'
+    else
+       redirect_to prototype_path(@prototype), alert: 'Tag was unsuccessfully deleted'
     end
   end
 
